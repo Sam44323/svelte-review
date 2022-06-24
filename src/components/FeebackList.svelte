f9b1a8bc23c15d1b32959ff5833ddc5acfedcb3a
Suranjan <suranjanbiswas2002@gmail.com>
2022-06-24 16:03:13 +0530

    add: added custom component for feedback_item

--- src/components/FeebackList.svelte
+++ src/components/FeebackList.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
+  import FeebackList from "./FeebackList.svelte";
+  import FeebackItem from "./FeebackItem.svelte";
   export let feedbackData = []; // will be props (if anything is not passed, then it'll be the default data)
 
   console.log(feedbackData);
 </script>
 
 <main>
-  <h1>FeebackList</h1>
+  {#each feedbackData as fData (fData.id)}
+    <h3>{fData.text}</h3>
+    <FeebackItem />
+  {/each}
 </main>
 
 <style></style>
