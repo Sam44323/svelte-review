4d491072cb5282202896aa6cb3ee2741e1773954
Suranjan <suranjanbiswas2002@gmail.com>
2022-06-24 15:57:27 +0530

    y

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,27 @@
 <script lang="ts">
+  import FeebackList from "./components/FeebackList.svelte";
+  import FeedbackList from "./components/FeebackList.svelte";
+  let feedback = [
+    {
+      id: 1,
+      rating: 10,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+    },
+  ];
 </script>
 
 <main />
-<h1>Hello!</h1>
+<FeebackList />
 
 <style>
 </style>
---
+++ src/components/FeebackList.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+</script>
+
+<main>
+  <h1>FeebackList</h1>
+</main>
+
+<style></style>
